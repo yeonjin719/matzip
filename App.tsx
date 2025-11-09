c64af9fa798fab0744c23774db5124cc9edf4691
김연진 <kyj0719@gmail.com>
2025-11-09 23:19:55 +0900

    feat: 수정

--- App.tsx
+++ App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
 import './global.css';
 import RootNavigation from './src/navigations/RootNavigation';
 
 function App() {
   return <RootNavigation />;
 }
-
-const styles = StyleSheet.create({});
 
 export default App;
--- src/components/CustomDrawerContent.tsx
+++ src/components/CustomDrawerContent.tsx
@@ -3,9 +3,8 @@ import {
   DrawerContentScrollView,
   DrawerItemList,
 } from '@react-navigation/drawer';
-import {Image, SafeAreaView, StyleSheet, View} from 'react-native';
+import {Image, SafeAreaView, View} from 'react-native';
 import {Text} from 'react-native-gesture-handler';
-import {colors} from '../constants/colors';
 
 function CustomDrawerContent(props: DrawerContentComponentProps) {
   return (
@@ -17,26 +16,10 @@ function CustomDrawerContent(props: DrawerContentComponentProps) {
         <DrawerItemList {...props} />
       </DrawerContentScrollView>
       <View className="flex-row justify-end p-[20] border-t-[1px] border-t-gray-200">
-        <Text style={styles.menuText}>설정</Text>
+        <Text className="font-[15px]">설정</Text>
       </View>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  bottomContainer: {
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    padding: 20,
-    borderTopWidth: 1,
-    borderTopColor: colors.GRAY_200,
-  },
-  menuText: {
-    fontSize: 15,
-  },
-});
 
 export default CustomDrawerContent;
